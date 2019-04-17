3a3d42fbf392aff92aa4dcb450cee712ab4c52a0
BorisRosaz <49713183+BorisRosaz@users.noreply.github.com>
2019-04-17 10:39:01 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -10,6 +10,7 @@
         require_once("object/account.php");
         require_once("object/site.php");
 
+        //Content :
         require_once("view/header.php");
         require_once("view/accueil.php");
         require_once("view/footer.php");
